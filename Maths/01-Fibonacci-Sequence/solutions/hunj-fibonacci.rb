2b8e25d0372cd5d012d059973ac7e86b6436fcae
Hun Jae Lee <hunjael@gmail.com>
2015-06-05 10:48:09 +0900

    solution added

---
+++ Maths/01-Fibonacci-Sequence/solutions/hunj-fibonacci.rb
@@ -0,0 +1,12 @@
+@fibonacci = [nil, 1, 1] # prevents 0th fibonacci
+
+def fibonacci(number)
+  return 1 if number == 1 or number == 2
+  return @fibonacci[number] if @fibonacci[number]
+  @fibonacci[number] = fibonacci(number-1) + fibonacci(number-2)
+  @fibonacci[number]
+end
+
+fibonacci(100)
+
+p @fibonacci
