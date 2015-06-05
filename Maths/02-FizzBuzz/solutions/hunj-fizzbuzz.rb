6076efa05f8ef5910eb49d2b3efefb765a6d3289
Hun Jae Lee <hunjael@gmail.com>
2015-06-05 09:54:59 +0900

    added @hunj's solution

---
+++ Maths/02-FizzBuzz/solutions/hunj-fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+  if number % 5 == 0 and number % 3 == 0
+    puts "FizzBuzz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  else
+    puts number
+  end
+end
+
+(1..100).each do |n|
+  fizzbuzz n
+end
