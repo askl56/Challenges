501f8ccfd1a9950e38023976407f3b0d26104ef2
Hun Jae Lee <hunjael@gmail.com>
2015-06-10 10:26:46 +0900

    Added @hunj's solution

---
+++ Maths/08-Hailstone-Sequence/solutions/hunj-hailstone.rb
@@ -0,0 +1,35 @@
+class Fixnum
+  def hailstone
+    arr = []
+    n = self
+    arr << n
+    while n != 1
+      if n % 2 == 0
+        arr << n/2
+        n /= 2
+      else
+        arr << (3 * n + 1)
+        n = 3 * n + 1
+      end
+    end
+    arr
+  end
+end
+
+
+sequences = {}
+number = 0
+max = 0
+
+
+(1...1e5).each do |num|
+  sequences[num] = num.hailstone.size
+
+  if sequences[num] > max
+    number = num
+    max = sequences[num]
+  end
+end
+
+p number # => 77031
+p sequences[number] # => 351
