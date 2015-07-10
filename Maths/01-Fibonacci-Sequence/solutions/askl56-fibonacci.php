3e2769f18b133389083c5be77b9534d40b8c9705
askl56 <amscott@openmailbox.org>
2015-07-10 21:33:17 +0100

    hailstone py

--- Maths/01-Fibonacci-Sequence/solutions/askl56-fibonacci.php
+++ Maths/01-Fibonacci-Sequence/solutions/askl56-fibonacci.php
@@ -1,11 +1,13 @@
+<?php
 function fibIter($n) {
-    if ($n < 2) {
-        return $n;
-    }
-    $fibPrev = 0;
-    $fib = 1;
-    foreach (range(1, $n-1) as $i) {
-        list($fibPrev, $fib) = array($fib, $fib + $fibPrev);
-    }
-    return $fib;
+  if ($n < 2) {
+    return $n;
+  }
+  $fibPrev = 0;
+  $fib = 1;
+  foreach (range(1, $n-1) as $i) {
+    list($fibPrev, $fib) = array($fib, $fib + $fibPrev);
+  }
+  return $fib;
 }
+?>
