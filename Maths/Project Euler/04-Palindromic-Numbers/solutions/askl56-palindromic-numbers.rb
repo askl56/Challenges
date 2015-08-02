a79fa96d25df50dd5e5fb7629be4b2d6c3ae1319
askl56 <amscott@openmailbox.org>
2015-08-02 12:40:01 +0100

    added palindromic numbers solution

---
+++ Maths/Project Euler/04-Palindromic-Numbers/solutions/askl56-palindromic-numbers.rb
@@ -0,0 +1,27 @@
+# Euler 4
+# http://projecteuler.net/index.php?section=problems&id=4
+# Find the largest palindrome made from the product of
+# two 3-digit numbers. A palindromic number reads the
+# same both ways. The largest palindrome made from the
+# product of two 2-digit numbers is 9009 = 91 x 99.
+# Find the largest palindrome made from the product of
+# two 3-digit numbers.
+
+
+timer_start = Time.now
+
+def is_palindrome?(n)
+  n.to_s == n.to_s.reverse
+end
+
+max = 0
+100.upto(999) { |i|
+  i.upto(999) { |j|
+    p = i * j
+
+    if is_palindrome?(p) && p > max then max = p end
+  }
+}
+
+puts max #906609
+puts "Elapsed Time: #{(Time.now - timer_start)*1000} milliseconds"
