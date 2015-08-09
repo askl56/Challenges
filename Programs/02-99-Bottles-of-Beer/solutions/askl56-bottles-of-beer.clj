f7ca74323c8edf23eb9590bb66049bf202f8917f
askl56 <amscott@openmailbox.org>
2015-08-09 15:18:25 +0100

    fixed indentation

--- Programs/02-99-Bottles-of-Beer/solutions/askl56-bottles-of-beer.clj
+++ Programs/02-99-Bottles-of-Beer/solutions/askl56-bottles-of-beer.clj
@@ -2,11 +2,11 @@
   [start]
   (doseq [n (range start 0 -1)]
     (printf "%d bottles of beer on the wall,
-%d bottles of beer,
-Take one down, pass it around,
-%d bottles of beer on the wall.\n\n"
-          n
-          n
-          (dec n))))
+      %d bottles of beer,
+      Take one down, pass it around,
+      %d bottles of beer on the wall.\n\n"
+      n
+      n
+      (dec n))))
 
 (sing 99)
