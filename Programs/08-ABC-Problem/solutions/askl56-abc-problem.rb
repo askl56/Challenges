d45a06285bd761bf3679205ba0950c920ea28157
askl56 <amscott@openmailbox.org>
2015-09-11 10:08:18 +0100

    added ruby ABC challenge

---
+++ Programs/08-ABC-Problem/solutions/askl56-abc-problem.rb
@@ -0,0 +1,16 @@
+words = %w(A BaRK BOoK tREaT COmMOn SqUAD CoNfuSE) << ""
+
+words.each do |word|
+  blocks = "BO XK DQ CP NA GT RE TG QD FS JW HU VI AN OB ER FS LY PC ZM"
+  res = word.each_char.all?{|c| blocks.sub!(/\w?#{c}\w?/i, "")}  #regexps can be interpolated like strings
+  puts "#{word.inspect}: #{res}"
+end
+
+"A": true
+"BaRK": true
+"BOoK": false
+"tREaT": true
+"COmMOn": false
+"SqUAD": true
+"CoNfuSE": true
+"": true
